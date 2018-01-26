63c59ecd013ff7eeecb87a6cb3cf56cb6355b31a
Andreas Hiltenkamp <andreas.hiltenkamp@1und1.de>
2018-01-26 05:21:33 +0100

    Initial Version

---
+++ iOS Splitview Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Splitview Sample
+//
+//  Created by Andreas Hiltenkamp on 26.01.18.
+//  Copyright © 2018 Andreas Hiltenkamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
